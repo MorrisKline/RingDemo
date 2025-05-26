ccb5956ca9097f9c24ec7e71f11d33eec3ef8126
MorrisKline <Morris1Kline@outlook.com>
2025-05-26 17:53:06 -0400

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use ring::digest;
 
 fn main() {
-    
+    // 待哈希的数据
     let data = b"Hello, world!";
     
+    // 计算 SHA-256 哈希值
     let hash: digest::Digest = digest::digest(&digest::SHA256, data);
 
-    print!("SHA-256 hash: ");
-    for byte in hash.as_ref() {
-        print!("{:02x}", byte);
-    }
-    println!();
+    // 将字节切片转为十六进制字符串
+    let hex_string: String = hash.as_ref().iter().map(|byte| format!("{:02x}", byte)).collect();
+
+    // 打印哈希值
+    println!("SHA-256 hash: {}", hex_string);
 }
